ee8c016e015415a4ab835381d83ba0e0c68db012
Nazmul Hasan Robin <robin.sust08@gmail.com>
2022-02-09 00:02:06 +0600

    Popular plugin stat added

--- app/Http/Controllers/WordPressStatController.php
+++ app/Http/Controllers/WordPressStatController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 
 class WordPressStatController extends Controller
 {
     //
+    protected $wpdevPlugins = [];
+    protected $wpdevPluginsDetails = [];
     /**
      * Display a listing of the resource.
      *
@@ -19,122 +23,30 @@ class WordPressStatController extends Controller
         // $response = Http::get($wp_org_api_url);
         // $response = Http::get('api.wordpress.org');
         // dd($response);
+        
+        
+        $wpdevPlugins = Cache::get('wpdevPlugins');
+        $wpdevPluginsDetails = Cache::get('wpdevPluginsDetails');
 
-        $api_params = [
-            // 'user_agent' => 'RobinWPDeveloper/1.0',
-            'request[search]' => 'wpdeveloper', /// <<== searched keyword
-            'request[page]' => 1,
-            'request[per_page]' => 100,
-            // 'request[browse]' => 'top-rated',
-            'request[browse]' => 'popular',
-            // 'request[author]' => '<a href="https://wpdeveloper.com/">WPDeveloper</a>',
-            //===Author and browse doesn't work together===
-            // This is a great idea to only fetch fields that are needed.
-            'request[fields]' => [
-                'name' => true,
-                'author' => true,
-                'slug' => true,
-                'downloadlink' => true,
-        
-                // we don't care about these at all so we want less data for faster transfer
-                'rating' => false,
-                'ratings' => false,
-                'downloaded' => true,
-                'description' => false,
-                'active_installs' => true,
-                'short_description' => false,
-                'donate_link' => false,
-                'tags' => false,
-                'sections' => false,
-                'homepage' => true,
-                'added' => false,
-                'last_updated' => false,
-                'compatibility' => false,
-                'tested' => false,
-                'requires' => false,
-                'versions' => false,
-                'support_threads' => false,
-                'support_threads_resolved' => false,
-            ],
-        ];
-        
-        /////////////////////////////////////////////////////////////////////////
-        // Fetching plugins
-        $plugin_search_api_url = 'http://api.wordpress.org/plugins/info/1.1/?action=query_plugins';
-        $api_url = $plugin_search_api_url;
-        $packaged_params = $api_params;
-        $packaged_params['request']['search'] = 'wpdeveloper';
-        $packaged_params = http_build_query($packaged_params);
-        
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $api_url);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $packaged_params);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); // hmm?
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // hmm?
-        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-        $content_json_maybe = curl_exec($ch);
-        $error = curl_error($ch);
-        
-        if ( !empty( $error ) ) {
-            echo "Error: $error\n";
-            $result = curl_getinfo($ch);
-        } else {
-            $result = json_decode( $content_json_maybe, true );
-        }
-        
-        $popularPlugins = !empty($result['plugins']) ? $result['plugins'] : []; 
-        foreach ($popularPlugins as $key => $popularPlugin) {
-            // dd($popularPlugin);
-            $popularRanking = $key + 1;
-            if($popularPlugin['author_profile'] == 'https://profiles.wordpress.org/wpdevteam/') {
-                echo $popularRanking . ' - ' .  $popularPlugin['name'] . '<br>';
+        if($wpdevPlugins) {
+            //Fetch from cache
+            $this->wpdevPlugins = $wpdevPlugins;
+            $this->wpdevPluginsDetails = $wpdevPluginsDetails;
+        }else {
+            //Fetch from API
+            for($i=1;$i<=20;$i++){
+                $this->popular_plugins($i);
             }
-        }
-        curl_close($ch);
 
-        dd($popularPlugins);
-        /////////////////////////////////////////////////////////////////////////
-        
-        
-        
-        /////////////////////////////////////////////////////////////////////////
-        // Fetching themes
-        $theme_search_api_url = 'http://api.wordpress.org/themes/info/1.1/?action=query_themes';
-        $api_url = $theme_search_api_url;
-        $packaged_params = $api_params;
-        $packaged_params['request']['search'] = 'ocean';
-        $packaged_params = http_build_query($packaged_params);
-        
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $api_url);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $packaged_params);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); // hmm?
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // hmm?
-        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-        $content_json_maybe = curl_exec($ch);
-        $error = curl_error($ch);
-        
-        if ( !empty( $error ) ) {
-            echo "Error: $error\n";
-            $result = curl_getinfo($ch);
-        } else {
-            $result = json_decode( $content_json_maybe, true );
+            $expiresAt = Carbon::now()->addDay(1);
+            Cache::put('wpdevPlugins', $this->wpdevPlugins, $expiresAt);
+            Cache::put('wpdevPluginsDetails', $this->wpdevPluginsDetails, $expiresAt);
         }
         
-        dd($result);
+        $data['wpdevPlugins'] = $this->wpdevPlugins;
+        $data['wpdevPluginsDetails'] = $this->wpdevPluginsDetails;
         
-        curl_close($ch);
-        /////////////////////////////////////////////////////////////////////////
-        
-        
-        exit(0);
-
-
-        return view('wordpress', compact('response'));
+        return view('wordpress', $data);
     }
 
     /**
@@ -201,5 +113,86 @@ class WordPressStatController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    public function popular_plugins($page_number = 1) {
+        $api_params = [
+            // 'user_agent' => 'RobinWPDeveloper/1.0',
+            'request[search]' => 'wpdeveloper', /// <<== searched keyword
+            'request[page]' => $page_number,
+            'request[per_page]' => 100,
+            // 'request[browse]' => 'top-rated',
+            'request[browse]' => 'popular',
+            // 'request[author]' => 'wpdevteam',
+            //===Author and browse doesn't work together===
+            // This is a great idea to only fetch fields that are needed.
+            'request[fields]' => [
+                'name' => true,
+                'author' => true,
+                'slug' => true,
+                'downloadlink' => true,
+        
+                // we don't care about these at all so we want less data for faster transfer
+                'rating' => false,
+                'ratings' => false,
+                'downloaded' => true,
+                'description' => false,
+                'active_installs' => true,
+                'short_description' => false,
+                'donate_link' => false,
+                'tags' => false,
+                'sections' => false,
+                'homepage' => true,
+                'added' => false,
+                'last_updated' => false,
+                'compatibility' => false,
+                'tested' => false,
+                'requires' => false,
+                'versions' => false,
+                'support_threads' => false,
+                'support_threads_resolved' => false,
+            ],
+        ];
+        
+        /////////////////////////////////////////////////////////////////////////
+        // Fetching plugins
+        $plugin_search_api_url = 'http://api.wordpress.org/plugins/info/1.1/?action=query_plugins';
+        $api_url = $plugin_search_api_url;
+        $packaged_params = $api_params;
+        $packaged_params['request']['search'] = 'wpdeveloper';
+        $packaged_params = http_build_query($packaged_params);
+        
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $api_url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $packaged_params);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); // hmm?
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // hmm?
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        $content_json_maybe = curl_exec($ch);
+        $error = curl_error($ch);
+        
+        if ( !empty( $error ) ) {
+            echo "Error: $error\n";
+            $result = curl_getinfo($ch);
+        } else {
+            $result = json_decode( $content_json_maybe, true );
+        }
+        
+        $popularPlugins = !empty($result['plugins']) ? $result['plugins'] : []; 
+        foreach ($popularPlugins as $key => $popularPlugin) {
+            $popularRanking = ($page_number-1) *100 + $key + 1;
+            if($popularPlugin['author_profile'] == 'https://profiles.wordpress.org/wpdevteam/') {
+                
+                //Not our plugin
+                if($popularPlugin['slug'] == 'disable-feeds') {
+                    continue;
+                }
+                $this->wpdevPluginsDetails[$popularRanking] = $popularPlugin;
+                $this->wpdevPlugins[$popularRanking] = $popularPlugin['name'];
+            }
+        }
+        curl_close($ch);
     }
 }
--- resources/views/wordpress.blade.php
+++ resources/views/wordpress.blade.php
@@ -19,11 +19,54 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    Popular Plugins - WPDeveloper 
+                    Popular Plugins - WPDeveloper
                 </div>
-                <ul>
-                    <li></li>
-                </ul>
+                <div class="pb-3"></div>
+                <div class="pl-3">
+                    <style>
+                        th, td {
+                            vertical-align: middle;
+                            text-align: center;
+                        }
+                    </style>
+                    <table class="w-full whitespace-nowrap">
+                        <thead>
+                            <tr class="focus:outline-none h-16 border border-gray-100 rounded">
+                                <th>Rank</th>
+                                <th>Active Installs</th>
+                                <th width="35%">Plugin</th>
+                                <th>Total Downloads</th>
+                                <th>Version</th>
+                                <th>Others</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach( $wpdevPluginsDetails as $key => $plugin)
+                            <tr class="focus:outline-none h-16 border border-gray-100 rounded">
+                                <td>
+                                    {{ $key }}
+                                </td>
+                                <td>
+                                    {{ number_format($plugin['active_installs'], 2) }}
+                                </td>
+                                <td>
+                                    <a href="{{ $plugin['homepage'] }}">{{ $plugin['name'] }}</a>
+                                </td>
+                                <td>
+                                    {{ number_format($plugin['downloaded'], 2) }}
+                                </td>
+                                <td>
+                                    {{ $plugin['version'] }}
+                                </td>
+                                <td>
+                                    Total Ratings: {{ $plugin['num_ratings'] }} <br>
+                                    Support Threads Resolved: {{ $plugin['support_threads_resolved'] }}/{{ $plugin['support_threads'] }} <br>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>

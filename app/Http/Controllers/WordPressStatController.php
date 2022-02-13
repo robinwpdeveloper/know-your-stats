4addf75812986994cc14c113b32b6be8b62cea74
Nazmul Hasan Robin <robin.sust08@gmail.com>
2022-02-13 07:36:30 +0600

    Download history table added

--- app/Http/Controllers/WordPressStatController.php
+++ app/Http/Controllers/WordPressStatController.php
@@ -12,6 +12,10 @@ class WordPressStatController extends Controller
     //
     protected $wpdevPlugins = [];
     protected $wpdevPluginsDetails = [];
+    protected $wpdevPluginsDownloadsDetails = [];
+    protected $wpdevPluginsDownloads = [];
+    protected $wpdevAllPlugins = [];
+    protected $wpdevAllPluginsDetails = [];
     /**
      * Display a listing of the resource.
      *
@@ -31,6 +35,14 @@ class WordPressStatController extends Controller
         $wpdevPlugins = Cache::get('wpdevPlugins');
         $wpdevPluginsDetails = Cache::get('wpdevPluginsDetails');
 
+        $wpdevAllPlugins = Cache::get('wpdevAllPlugins');
+        $wpdevAllPluginsDetails = Cache::get('wpdevAllPluginsDetails');
+
+        $wpdevPluginsDownloads = Cache::get('wpdevPluginsDownloads');
+        $wpdevPluginsDownloadsDetails = Cache::get('wpdevPluginsDownloadsDetails'); 
+
+        $expiresAt = Carbon::now()->addDay(1);
+        
         if($wpdevPlugins) {
             //Fetch from cache
             $this->wpdevPlugins = $wpdevPlugins;
@@ -41,13 +53,35 @@ class WordPressStatController extends Controller
                 $this->popular_plugins($i);
             }
 
-            $expiresAt = Carbon::now()->addDay(1);
             Cache::put('wpdevPlugins', $this->wpdevPlugins, $expiresAt);
             Cache::put('wpdevPluginsDetails', $this->wpdevPluginsDetails, $expiresAt);
+        }
+
+        if($wpdevPluginsDownloads) {
+            //Fetch from cache
+            $this->wpdevAllPlugins = $wpdevAllPlugins;
+            $this->wpdevAllPluginsDetails = $wpdevAllPluginsDetails;
+            $this->wpdevPluginsDownloads = $wpdevPluginsDownloads;
+            $this->wpdevPluginsDownloadsDetails = $wpdevPluginsDownloadsDetails;
+        }else {
+            //Fetch from API
+            $this->author_plugins();
+            foreach ($this->wpdevAllPlugins as $pluginSlug => $pluginName) {
+                $this->plugin_download_history($pluginSlug);
+            }
+            
+            Cache::put('wpdevAllPlugins', $this->wpdevAllPlugins, $expiresAt);
+            Cache::put('wpdevAllPluginsDetails', $this->wpdevAllPluginsDetails, $expiresAt);
+            Cache::put('wpdevPluginsDownloads', $this->wpdevPluginsDownloads, $expiresAt);
+            Cache::put('wpdevPluginsDownloadsDetails', $this->wpdevPluginsDownloadsDetails, $expiresAt);
         }
         
         $data['wpdevPlugins'] = $this->wpdevPlugins;
         $data['wpdevPluginsDetails'] = $this->wpdevPluginsDetails;
+        $data['wpdevAllPlugins'] = $this->wpdevAllPlugins;
+        $data['wpdevAllPluginsDetails'] = $this->wpdevAllPluginsDetails;
+        $data['wpdevPluginsDownloads'] = $this->wpdevPluginsDownloads;
+        $data['wpdevPluginsDownloadsDetails'] = $this->wpdevPluginsDownloadsDetails;
         
         return view('wordpress', $data);
     }
@@ -194,6 +228,123 @@ class WordPressStatController extends Controller
                 }
                 $this->wpdevPluginsDetails[$popularRanking] = $popularPlugin;
                 $this->wpdevPlugins[$popularRanking] = $popularPlugin['name'];
+            }
+        }
+        curl_close($ch);
+    }
+    
+    public function plugin_download_history($pluginSlug = 'better-payment', $limit = 730) {
+        // Fetching plugins
+        $plugin_search_api_url = "https://api.wordpress.org/stats/plugin/1.0/downloads.php?slug=$pluginSlug&limit=$limit";
+        $api_url = $plugin_search_api_url;
+        
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $api_url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); // hmm?
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // hmm?
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        $content_json_maybe = curl_exec($ch);
+        $error = curl_error($ch);
+        
+        if ( !empty( $error ) ) {
+            echo "Error: $error\n";
+            $result = curl_getinfo($ch);
+        } else {
+            $result = json_decode( $content_json_maybe, true );
+        }
+
+        $pluginDownloads = !empty($result) ? $result : []; 
+        $this->wpdevPluginsDownloadsDetails[$pluginSlug] = $pluginDownloads;
+
+        //all time records 
+        sort($pluginDownloads);
+        $maxDownloadAllTime = max($pluginDownloads);
+        $max2ndDownloadAllTime = $pluginDownloads[sizeof($pluginDownloads)-2];
+    
+        // 2nd all time records
+        $this->wpdevPluginsDownloads[$pluginSlug]['maxDownloadAllTime'] = $maxDownloadAllTime;
+        $this->wpdevPluginsDownloads[$pluginSlug]['max2ndDownloadAllTime'] = $max2ndDownloadAllTime;
+        curl_close($ch);
+    }
+
+    public function author_plugins($page_number = 1, $author_slug = 'wpdevteam') {
+        $api_params = [
+            // 'user_agent' => 'RobinWPDeveloper/1.0',
+            'request[search]' => 'wpdeveloper', /// <<== searched keyword
+            'request[page]' => $page_number,
+            'request[per_page]' => 100,
+            // 'request[browse]' => 'top-rated',
+            // 'request[browse]' => 'popular',
+            'request[author]' => $author_slug,
+            //===Author and browse doesn't work together===
+            // This is a great idea to only fetch fields that are needed.
+            'request[fields]' => [
+                'name' => true,
+                'author' => true,
+                'slug' => true,
+                'downloadlink' => true,
+        
+                // we don't care about these at all so we want less data for faster transfer
+                'rating' => false,
+                'ratings' => false,
+                'downloaded' => true,
+                'description' => false,
+                'active_installs' => true,
+                'short_description' => false,
+                'donate_link' => false,
+                'tags' => false,
+                'sections' => false,
+                'homepage' => true,
+                'added' => false,
+                'last_updated' => false,
+                'compatibility' => false,
+                'tested' => false,
+                'requires' => false,
+                'versions' => false,
+                'support_threads' => false,
+                'support_threads_resolved' => false,
+            ],
+        ];
+        
+        /////////////////////////////////////////////////////////////////////////
+        // Fetching plugins
+        $plugin_search_api_url = 'http://api.wordpress.org/plugins/info/1.1/?action=query_plugins';
+        $api_url = $plugin_search_api_url;
+        $packaged_params = $api_params;
+        $packaged_params['request']['search'] = 'wpdeveloper';
+        $packaged_params = http_build_query($packaged_params);
+        
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $api_url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $packaged_params);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); // hmm?
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // hmm?
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        $content_json_maybe = curl_exec($ch);
+        $error = curl_error($ch);
+        
+        if ( !empty( $error ) ) {
+            echo "Error: $error\n";
+            $result = curl_getinfo($ch);
+        } else {
+            $result = json_decode( $content_json_maybe, true );
+        }
+        
+        $allPlugins = !empty($result['plugins']) ? $result['plugins'] : []; 
+        foreach ($allPlugins as $key => $allPlugin) {
+            
+            if($allPlugin['author_profile'] == 'https://profiles.wordpress.org/wpdevteam/') {
+                
+                //Not our plugin
+                if($allPlugin['slug'] == 'disable-feeds') {
+                    continue;
+                }
+                $this->wpdevAllPluginsDetails[$allPlugin['slug']] = $allPlugin;
+                $this->wpdevAllPlugins[$allPlugin['slug']] = $allPlugin['name'];
             }
         }
         curl_close($ch);
--- resources/views/wordpress.blade.php
+++ resources/views/wordpress.blade.php
@@ -54,6 +54,9 @@
                                 </td>
                                 <td>
                                     {{ number_format($plugin['downloaded'], 2) }}
+
+                                    <?php $wpdevPluginsDownloads[$plugin['slug']]['pluginDetails'] = $plugin; ?>
+                                    <?php $wpdevPluginsDownloads[$plugin['slug']]['downloaded'] = number_format($plugin['downloaded'], 2); ?>
                                 </td>
                                 <td>
                                     {{ $plugin['version'] }}
@@ -61,6 +64,78 @@
                                 <td>
                                     Ratings: {{ $plugin['num_ratings'] }} <br>
                                     Support: {{ $plugin['support_threads_resolved'] }}/{{ $plugin['support_threads'] }} <br>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                <div class="pb-3"></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    Download History - WPDeveloper (Last 2 years)
+                </div>
+                <div class="pb-3"></div>
+                <div class="pl-3" style="padding-right: 0.75rem">
+                    <style>
+                        th, td {
+                            vertical-align: middle;
+                            text-align: center;
+                        }
+                    </style>
+                    <table class="whitespace-nowrap">
+                        <thead>
+                            <tr class="focus:outline-none h-16 border border-gray-100 rounded">
+                                <th>Most Downloads/Day (Lifetime)</th>
+                                <th>2nd Most Downloads/Day (Lifetime)</th>
+                                <th>Most Downloads/Day (Current Month - February)</th>
+                                <th>Most Downloads/Day (Current Year - 2022)</th>
+                                <th width="35%">Downloads/Day (Today)</th>
+                                <th>Downloads/Day (Yesterday)</th>
+                                <th>Total Downloads</th>
+                                <th>Other</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach( $wpdevPluginsDownloads as $slug => $downloadHistory)
+                            <tr class="focus:outline-none h-16 border border-gray-100 rounded">
+                                <td>
+                                    {{ isset($downloadHistory['maxDownloadAllTime']) ? $downloadHistory['maxDownloadAllTime'] : '-' }}
+                                </td>
+                                <td>
+                                    {{ isset($downloadHistory['max2ndDownloadAllTime']) ? $downloadHistory['max2ndDownloadAllTime'] : '-' }}
+                                </td>
+                                <td>
+                                    <a href="{{ $plugin['homepage'] }}">{{ $slug }}</a>
+                                </td>
+                                <td>
+                                    {{ $slug }}
+                                </td>
+                                <td>
+                                    <?php 
+                                    date_default_timezone_set('America/Los_Angeles');
+                                    $today = date("Y-m-d", time());
+                                    $yesterday = date('Y-m-d',strtotime("-1 days"));
+
+                                    $slugDownloadHistory = $wpdevPluginsDownloadsDetails[$slug]; ?>
+
+                                    {{ isset($slugDownloadHistory[$today]) ? $slugDownloadHistory[$today] : '-' }}
+                                </td>
+                                <td>
+                                    {{ isset($slugDownloadHistory[$yesterday]) ? $slugDownloadHistory[$yesterday] : '-' }}
+                                </td>
+                                <td>
+                                    {{ isset($downloadHistory['downloaded']) ? $downloadHistory['downloaded'] : '-' }}
+                                </td>
+                                <td>
+                                    Slug: {{ $slug }} <br>
+                                    Name: {{ isset($downloadHistory['pluginDetails']['name']) ? $downloadHistory['pluginDetails']['name'] : '-' }} <br>
                                 </td>
                             </tr>
                             @endforeach
